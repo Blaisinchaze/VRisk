5e0a66f2362d43f94ba5890adc5c541e0e02221d
Simone <simone2.farinelli@live.uwe.ac.uk>
2023-05-05 18:45:33 +0100

    Small Tweak
    ID of buildings can be seen in hierarchy

--- Assets/Editor/IDSetter.cs
+++ Assets/Editor/IDSetter.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class IDSetter : EditorWindow
 {
-    private string safe_risk_tag   = "Black";
-    private string low_risk_tag    = "Beige";
-    private string mid_risk_tag    = "Yellow";
-    private string high_risk_tag   = "Red";
-    
+    private string safe_risk_tag = "Black";
+    private string low_risk_tag = "Beige";
+    private string mid_risk_tag = "Yellow";
+    private string high_risk_tag = "Red";
+
     [MenuItem("Window/ID Setter")]
     static void Init()
     {
@@ -20,14 +20,14 @@ public class IDSetter : EditorWindow
     private void OnGUI()
     {
         GUILayout.Label("Risk Tags", EditorStyles.boldLabel);
-        
+
         EditorGUILayout.Space(10);
-        
+
         EditorGUILayout.TextField("Safe tag: ", safe_risk_tag);
         EditorGUILayout.TextField("Low risk tag: ", low_risk_tag);
         EditorGUILayout.TextField("Mid risk tag: ", mid_risk_tag);
         EditorGUILayout.TextField("High risk tag: ", high_risk_tag);
-        
+
         EditorGUILayout.Space(10);
 
         if (GUILayout.Button("Give Buildings IDs")) SetIDs(false);
@@ -38,27 +38,36 @@ public class IDSetter : EditorWindow
     {
         GameObject[][] buildings = new GameObject[4][];
         int current_id = 0;
-        
+
         buildings[0] = GameObject.FindGameObjectsWithTag(safe_risk_tag);
         buildings[1] = GameObject.FindGameObjectsWithTag(low_risk_tag);
         buildings[2] = GameObject.FindGameObjectsWithTag(mid_risk_tag);
         buildings[3] = GameObject.FindGameObjectsWithTag(high_risk_tag);
 
-        foreach (var groups in buildings)
+        for (int i = 0; i < 4; i++)
         {
-            foreach (var building in groups)
+            foreach (var building in buildings[i])
             {
                 var building_data = building.GetComponent<BuildingData>();
                 
-                if (!reset)
+                switch (i)
                 {
-                    building_data.id = current_id;
-                    current_id++;
+                    case 0:
+                        building.name = "SafeBuilding(" + current_id + ")";
+                        break;
+                    case 1:
+                        building.name = "LowRiskBuilding(" + current_id + ")";
+                        break;
+                    case 2:
+                        building.name = "MidRiskBuilding(" + current_id + ")";
+                        break;
+                    case 3:
+                        building.name = "HighRiskBuilding(" + current_id + ")";
+                        break;
                 }
-                else
-                {
-                    building_data.id = 0;
-                }
+
+                building_data.id = reset ? 0 : current_id;
+                current_id++;
             }
         }
     }
