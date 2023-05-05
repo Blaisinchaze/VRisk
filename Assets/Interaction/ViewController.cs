e070997fee66598f1734713df728bcf86b0aa8d6
Jade <jade2.eames@live.uwe.ac.uk>
2023-05-05 12:01:18 +0100

    View rotation by right joystick

--- Assets/Interaction/ViewController.cs
+++ Assets/Interaction/ViewController.cs
@@ -9,7 +9,10 @@ public class ViewController : MonoBehaviour
 {
     private InputAction view_action;
     public float rotate_speed = 2f;
-    
+
+    public Transform cam_trans;
+    public Transform rotation_parent_trans;
+
     private void Start()
     {
         view_action = InputHandler.Instance.input_asset.InputActionMap.RotateView;
@@ -18,15 +21,18 @@ public class ViewController : MonoBehaviour
     private void FixedUpdate()
     {
         Vector2 input_vect = view_action.ReadValue<Vector2>();
-
+        
         if (input_vect.y != 0)
         {
-            transform.Rotate(input_vect.y * rotate_speed * -1, 0f, 0f, Space.Self);
+            Quaternion cam_y_rotation = Quaternion.Euler(0, cam_trans.eulerAngles.y, 0);
+            Vector3 rotated_x_axis = cam_y_rotation * Vector3.right;
+
+            rotation_parent_trans.RotateAround(cam_trans.position, rotated_x_axis, input_vect.y * rotate_speed * -1); 
         }
 
         if (input_vect.x != 0)
         {
-            transform.Rotate(0f, input_vect.x * rotate_speed, 0f, Space.World);
+           transform.Rotate(0f, input_vect.x * rotate_speed, 0f, Space.World);
         }
     }
 }
