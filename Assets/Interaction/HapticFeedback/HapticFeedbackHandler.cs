8c3ee1a491262d83c538ce0a9a20e56c4c3a70ee
Jade <jade2.eames@live.uwe.ac.uk>
2023-05-09 21:36:17 +0100

    Increased player mass to avoid flying
    colliding hand with chest resulted in flying with default mass of 1kg

--- Assets/Interaction/HapticFeedback/HapticFeedbackHandler.cs
+++ Assets/Interaction/HapticFeedback/HapticFeedbackHandler.cs
@@ -15,18 +15,22 @@ public class HapticFeedbackHandler : MonoBehaviour
     public XRBaseController left_controller;
     public XRBaseController right_controller;
 
-    public void TriggerHapticFeedback(HapticFeedbackControllerID _controller, float _intensity, float _duration)
+    public void triggerHapticFeedback(HapticFeedbackControllerID _controller, float _intensity, float _duration)
     {
+        Debug.Log("hnjguowsrlaeazgnrojeakgnhjrjoea[h");
         switch (_controller)
         {
             case HapticFeedbackControllerID.LEFT:
             {
                 left_controller.SendHapticImpulse(_intensity, _duration);
+                Debug.Log("LEFT");
                 break;
             }
             case HapticFeedbackControllerID.RIGHT:
             {
                 right_controller.SendHapticImpulse(_intensity, _duration);
+                Debug.Log("RIGHT");
+
                 break;
             }
 
