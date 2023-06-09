32201b4c09fdce7ec06209e4f3b6f9065bccb825
Jade <jade2.eames@live.uwe.ac.uk>
2023-06-09 10:38:38 +0100

    Forgot a line

--- Assets/Debris/Debris_Prefabs/DebrisScript.cs
+++ Assets/Debris/Debris_Prefabs/DebrisScript.cs
@@ -10,6 +10,7 @@ public class DebrisScript : MonoBehaviour
     private void OnCollisionEnter(Collision other)
     {
         //ParticleID _id, Vector3 _location, Vector3 _rotation, Transform _parent = null, bool _relative_to_parent = false)
-        GameManager.Instance.ParticleManager.triggerEffect(ParticleManager.ParticleID.DEBRIS_IMPACT, other.contacts[0].point, other.contacts[0].normal);
+        GameManager.Instance.ParticleManager.triggerEffect(ParticleManager.ParticleID.DEBRIS_IMPACT, other.contacts[0].point,other.contacts[0].normal);
+        GameManager.Instance.AudioManager.PlaySound(false, false, other.contacts[0].point, AudioManager.SoundID.DEBRIS_COLLISION);
     }
 }
