3dc8f725f7779797dfba37c6d0934062cada2ca6
Simone <simone2.farinelli@live.uwe.ac.uk>
2023-06-16 16:31:41 +0100

    Improved anchor and menu, options menu can be accessed

--- Assets/GameManager/GameData.cs
+++ Assets/GameManager/GameData.cs
@@ -8,8 +8,17 @@ public class GameData : ScriptableObject
 {
     public int NextScene { get; set; }
 
+    public int Volume { get; set; }
+    public int Height { get; set; }
+
+    public float TurnSpeed { get; set; }
+
     private void Awake()
     {
         NextScene = 2;
+
+        Volume = 50;
+        Height = 170;
+        TurnSpeed = 5.0f;
     }
 }
--- Assets/Menus/UIAnchor.cs
+++ Assets/Menus/UIAnchor.cs
@@ -2,17 +2,23 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class UIAnchor : MonoBehaviour
 {
     public GameObject camera;
     
     [SerializeField] private float view_angle = 45.0f;
-    [SerializeField] private float lerp_speed = 5.0f;
+    [SerializeField] private float walk_area = 0.5f;
+    [SerializeField] private float lerp_speed_rot = 5.0f;
+    [SerializeField] private float lerp_speed_pos = 3.0f;
 
-    private bool reset = true;
+    private bool reset_angle = true;
+    private bool reset_pos = true;
     private float threshold = 0.05f;
+    
     private Vector3 default_pos;
+    private Vector3 anchor_pos;
     
     private void Awake()
     {
@@ -22,31 +28,51 @@ public class UIAnchor : MonoBehaviour
     private void Update()
     {
         Vector3 camera_pos = camera.transform.position;
-        Vector3 anchor_pos = new Vector3(camera_pos.x, default_pos.y + camera_pos.y, camera_pos.z);
-        transform.position = anchor_pos;
-
-        Quaternion camera_rot = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0);
-        Quaternion anchor_rot = Quaternion.Euler(0, transform.eulerAngles.y, 0);
-
-        if (reset)
+        anchor_pos = new Vector3(camera_pos.x, default_pos.y + camera_pos.y, camera_pos.z);
+        
+        if (reset_pos)
         {
-            if (Quaternion.Angle(anchor_rot, camera_rot) > threshold)
+            if (Vector3.Distance(transform.position, anchor_pos) >= 0.05f)
             {
-                anchor_rot = Quaternion.Lerp(anchor_rot, camera_rot, lerp_speed * Time.deltaTime);
+                transform.position = Vector3.Lerp(transform.position, anchor_pos, lerp_speed_pos * Time.deltaTime);
             }
             else
             {
-                reset = false;
+                reset_pos = false;
+            }
+        }
+        else
+        { 
+            if (!ApproximatelyEqual(transform.position, anchor_pos, walk_area)) reset_pos = true;
+        }
+        
+        Quaternion camera_rot = Quaternion.Euler(0, camera.transform.eulerAngles.y, 0);
+        Quaternion anchor_rot = Quaternion.Euler(0, transform.eulerAngles.y, 0);
+
+        if (reset_angle)
+        {
+            if (Quaternion.Angle(anchor_rot, camera_rot) > threshold)
+            {
+                anchor_rot = Quaternion.Lerp(anchor_rot, camera_rot, lerp_speed_rot * Time.deltaTime);
+            }
+            else
+            {
+                reset_angle = false;
             }
         }
         else
         {
-            if (Quaternion.Angle(anchor_rot, camera_rot) > view_angle)
-            {
-                reset = true;
-            }
+            if (Quaternion.Angle(anchor_rot, camera_rot) > view_angle) reset_angle = true;
+            
         }
 
         transform.rotation = anchor_rot;
+    }
+    
+    bool ApproximatelyEqual(Vector3 a, Vector3 b, float threshold)
+    {
+        return Mathf.Abs(a.x - b.x) <= threshold &&
+               Mathf.Abs(a.y - b.y) <= threshold &&
+               Mathf.Abs(a.z - b.z) <= threshold;
     }
 }
