8be61b36b067936c45777975c6dcd4ef42b4c89f
Jade <jade2.eames@live.uwe.ac.uk>
2023-06-18 17:53:55 +0100

    Win audio was a little funky - fixed now

--- Assets/SafeZones/SafeZoneScript.cs
+++ Assets/SafeZones/SafeZoneScript.cs
@@ -2,11 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class SafeZoneScript : MonoBehaviour
 {
+    public GameObject head;
     private void OnTriggerEnter(Collider other)
     {
+        GameManager.Instance.AudioManager.PlaySound(false, false, head.transform.position, AudioManager.SoundID.WIN);
         Debug.Log("transition");
     }
 }
