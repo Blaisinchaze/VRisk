ecdd32f0c999f8d85a0f5aa959654f404edcd0e0
Jade <jade2.eames@live.uwe.ac.uk>
2023-06-20 12:27:11 +0100

    Timer done - Just needs hooking up

--- Assets/TimerDisplay/TimerDisplay.cs
+++ Assets/TimerDisplay/TimerDisplay.cs
@@ -1,5 +1,5 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class TimerDisplay : MonoBehaviour
@@ -8,6 +8,69 @@ public class TimerDisplay : MonoBehaviour
     public ClockCharacter seconds_tens_char;
     public ClockCharacter minutes_ones_char;
     public ClockCharacter minutes_tens_char;
-    
-    
+
+    private int seconds;
+    private int minutes;
+
+    private bool active = false;
+
+    private void Start()
+    {
+        startTimerDisplay();
+    }
+
+    public void startTimerDisplay()
+    {
+        seconds = 0;
+        minutes = 0;
+
+        active = true;
+        StartCoroutine(updateTimer());
+    }
+
+    public void stopTimer()
+    {
+        active = false;
+    }
+
+    public void resetTimer()
+    {
+        seconds = 0;
+        minutes = 0;
+        
+        updateDisplay();
+    }
+
+    public void updateDisplay()
+    {
+        ClockCharacter.Character secondsOnes = (ClockCharacter.Character)(seconds % 10);
+        ClockCharacter.Character secondsTens = (ClockCharacter.Character)(seconds / 10);
+
+        ClockCharacter.Character minutesOnes = (ClockCharacter.Character)(minutes % 10);
+        ClockCharacter.Character minutesTens = (ClockCharacter.Character)(minutes / 10);
+
+        seconds_ones_char.setCharacter(secondsOnes);
+        seconds_tens_char.setCharacter(secondsTens);
+
+        minutes_ones_char.setCharacter(minutesOnes);
+        minutes_tens_char.setCharacter(minutesTens);
+    }
+
+    IEnumerator updateTimer()
+    {
+        while (active)
+        {
+            yield return new WaitForSeconds(1);
+
+            seconds++;
+
+            if (seconds >= 60)
+            {
+                minutes++;
+                seconds = 0;
+            }
+
+            updateDisplay();
+        }
+    }
 }
