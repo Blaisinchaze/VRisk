e486d2721430c6006a5d274b8bf42e78beb83113
Jade <jade2.eames@live.uwe.ac.uk>
2023-06-26 10:13:10 +0100

    Data can be unloaded, tabs are displayed correctly and can select/unselect all

--- Assets/ScrubThroughScripts/PlaythroughDataScript.cs
+++ Assets/ScrubThroughScripts/PlaythroughDataScript.cs
@@ -15,15 +15,6 @@ namespace DataVisualiser
 
         public List<TimelineElement> timeline;
         public bool survived;
-
-        public void setData(string _save_name, string _save_date, string _save_time, List<TimelineElement> _timeline, bool _survived)
-        {
-            save_name.text = _save_name;
-            save_date.text = _save_date;
-            save_time.text = _save_time;
-            timeline = _timeline;
-            survived = _survived;
-        }
     }
 
     [System.Serializable]
--- Assets/ScrubThroughScripts/UIHandler.cs
+++ Assets/ScrubThroughScripts/UIHandler.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 using SFB;
 using System.IO;
@@ -65,30 +64,18 @@ namespace DataVisualiser
 
             foreach (var file in csv_files)
             {
+                // Create playthrough tab and set display data (username, date, time).
                 var playthrough_data = createTab();
-                // instantiate and position button.
+                setTabDisplayData(playthrough_data, file);
 
+                // Grab the file contents and parse the csv.
                 string contents = FileManager.getFileContents(new[] {file});
                 string[][] csv_contents = FileManager.parseCSV(contents);
                 
-                bool survived = false;
-                string save_name = "";
-                string save_date = "";
-                string save_time = "";
-                List<TimelineElement> timeline = new List<TimelineElement>();
-                
-                survived = csv_contents[csv_contents.Length-2][3] == "Survived\r";
-                string filename = Path.GetFileNameWithoutExtension(file);
-                
-                string[] parts = filename.Split(new string[] { " - " }, StringSplitOptions.None);
-                string dateAndTimePart = parts[0];
-                save_name = parts.Length > 1 ? parts[1] : "";
-                
-                string[] dateAndTimeParts = dateAndTimePart.Split(new char[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
-                save_date = dateAndTimeParts.Length > 0 ? dateAndTimeParts[0].Trim() : "";
-                save_time = dateAndTimeParts.Length > 1 ? dateAndTimeParts[1].Trim() : "";
-                
+                // Set bool for whether or not player lived. 
+                playthrough_data.survived = csv_contents[csv_contents.Length-2][3] == "Survived\r";
 
+                // Store timeline points.
                 for (int line_index = 0; line_index < csv_contents.Length-1; line_index++)
                 {
                     string[] line = csv_contents[line_index];
@@ -96,29 +83,59 @@ namespace DataVisualiser
                     float time = float.Parse(line[0]);
                     Vector2 grid_cell = new Vector2(float.Parse(line[1]), float.Parse(line[2]));
                     
-                    timeline.Add(new TimelineElement(time, grid_cell));
+                    playthrough_data.timeline.Add(new TimelineElement(time, grid_cell));
                 }
-
-                playthrough_data.setData(save_name, save_date, save_time, timeline, survived);
-                // stash data, has to be tied to UI button.
-                // Have to create UI button to represent this data.
             }
         }
 
         private PlaythroughDataScript createTab()
         {
-            float y_position = -playthroughs.Count * (tab_height + tab_gap);
-
-            playthrough_button_container.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (tab_height + tab_gap) * playthroughs.Count);
+            // Expand container.
+            playthrough_button_container.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (tab_height + tab_gap) * (playthroughs.Count + 1) + tab_gap);
             
+            // Create new tab.
             GameObject playthrough_tab = Instantiate(playthrough_button_prefab, playthrough_button_container);
-            playthrough_tab.transform.localPosition = new Vector2(0, y_position);
-
             PlaythroughDataScript playthrough_data = playthrough_tab.GetComponent<PlaythroughDataScript>();
             
+            
+            // Add tab to list. 
             playthroughs.Add(new Pair<GameObject, PlaythroughDataScript>(playthrough_tab, playthrough_data));
             
             return playthrough_data;
+        }
+
+        private void setTabDisplayData(PlaythroughDataScript _script_data, string _file)
+        {
+            string filename = Path.GetFileNameWithoutExtension(_file);
+                
+            string[] parts = filename.Split(new string[] { " - " }, StringSplitOptions.None);
+            string dateAndTimePart = parts[0];
+            _script_data.name = parts.Length > 1 ? parts[1] : "";
+                
+            string[] dateAndTimeParts = dateAndTimePart.Split(new char[] { '(', ')' }, StringSplitOptions.RemoveEmptyEntries);
+            _script_data.save_date.text = dateAndTimeParts.Length > 0 ? dateAndTimeParts[0].Trim() : "";
+            _script_data.save_time.text = dateAndTimeParts.Length > 1 ? dateAndTimeParts[1].Trim() : "";
+        }
+        
+        public void UnloadData()
+        {
+            for (int i = playthroughs.Count-1; i > -1; --i)
+            {
+                Destroy(playthroughs[i].first);
+            }
+            
+            playthroughs.Clear();
+            playthrough_button_container.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, (tab_height + tab_gap) * (playthroughs.Count + 1) + tab_gap);
+
+        }
+
+
+        public void setSelectionForAll(bool _select)
+        {
+            foreach (var playthrough in playthroughs)
+            {
+                playthrough.second.selected.isOn = _select;
+            }
         }
     }
 }
